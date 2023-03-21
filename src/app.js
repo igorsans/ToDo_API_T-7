a2189b94535c1887361e306bb21a44f6576022b4
igorsans <igor.santos0@hotmail.com>
2023-03-20 22:13:14 -0300

    server adcionado

--- src/app.js
+++ src/app.js
@@ -3,13 +3,10 @@ import tarefasController from "./controllers/tarefasController.js";
 import usuariosController from "./controllers/usuariosController.js";
 
 const app = express()
+
 app.use(express.json())
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
-})
-
 
 tarefasController(app)
 usuariosController(app)
+
+export default app
