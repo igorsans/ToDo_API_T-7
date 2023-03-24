a138768d95f96b2a48c47c019eadf7f26126f5bb
igorsans <igor.santos0@hotmail.com>
2023-03-23 21:54:42 -0300

    ajustes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 import express from "express";
 import tarefasController from "./controllers/tarefasController.js";
 import usuariosController from "./controllers/usuariosController.js";
-import cors from 'cors'
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-    app.get("/testedeploy", (req, res) => {
-      res.status(200).send({msg: "Api sendo acessada remotamente"});
-    });
+app.get("/testedeploy", (req, res) => {
+  res.status(200).send({ msg: "Api sendo acessada remotamente" });
+});
 
-tarefasController(app)
-usuariosController(app)
+tarefasController(app);
+usuariosController(app);
 
-export default app
+export default app;
